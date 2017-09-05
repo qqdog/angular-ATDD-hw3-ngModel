6e9af82bc1fa082f76ee645dca9a9394f93ed175
qqdog <lianghuiru@lianghuirudeMacBook-Air.local>
2017-09-05 21:36:22 +0800

    驗收測試 (紅燈)

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,23 @@ describe('hw3-ng-model App', () => {
     page = new Hw3NgModelPage();
   });
 
-  it('should display message saying app works', () => {
+  it(`should have one button`, () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getButton().isPresent()).toBe(true);
   });
+
+  it(`should show text 'Submit' on the button`, () => {
+    page.navigateTo();
+    expect(page.getButtonText()).toBe('Submit');
+  });
+
+  it(`should have one checkbox`, () => {
+    page.navigateTo();
+    expect(page.getCheckBox().isPresent()).toBe(true);
+  });
+
+  // it(`should show text 'I agree' right from the checkbox`, () => {
+  //   page.navigateTo();
+  //   expect(page.getCheckBoxText()).toBe('I agree');
+  // });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,24 @@
 import { browser, element, by } from 'protractor';
 
 export class Hw3NgModelPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(): Hw3NgModelPage {
+    browser.get('/');
+    return this;
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getButton(): any {
+    return element(by.id('atdd-button'));
   }
+
+  getButtonText(): any {
+    return element(by.id('atdd-button')).getText();
+  }
+
+  getCheckBox(): any {
+    return element(by.id('atdd-checkbox'));
+  }
+
+  // getCheckBoxText(): any {
+  //   return element(by.id('atdd-checkbox'));
+  // }
 }
